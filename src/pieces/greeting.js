8257e1c78c803ab483f563872a1e6a81790bfbb1
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-11 17:59:50 -0500

    rendered a chip to display user feedback when trying to login

--- src/pieces/greeting.js
+++ src/pieces/greeting.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
+import React from 'react'
+import Dialog from 'material-ui/Dialog'
+import RaisedButton from 'material-ui/RaisedButton'
+import Chip from 'material-ui/Chip'
 
 export default class Greeting extends React.Component {
     constructor(props) {
         super(props)
-        
+
         this.state = {
-            open: true
+            open: true,
+            alert: false
         }
     }
 
+    renderChip() {
+      return this.state.alert ? (
+        <Chip onRequestDelete = { () => this.setState({alert: false}) }>
+          Login is not currently implemented
+        </Chip>
+      ) : ''
+    }
+
     render() {
+        const actions = [
+            <RaisedButton
+                label = "Login"
+                primary = { true }
+                onTouchTap = { () => { this.setState({alert: true}) } } >
+
+            </RaisedButton>
+        ]
+
         return (
             <div>
-                <Dialog title = "Welcome to our Chatroom!"
+                <Dialog title = "Welcome to the chatroom!"
                         modal = { true }
-                        open = { this.state.open }>
-                        Please select an action to continue!
+                        open = { this.state.open }
+                        actions = { actions }>
+                        Please press the button to continue { this.renderChip() }
                 </Dialog>
             </div>
         )
