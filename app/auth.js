7e183a1e24852c1258c7a172e6c1e73d176a4fab
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-12 12:38:14 -0500

    enforce being logged to use application
    however right now literally anyone can log in

--- app/auth.js
+++ app/auth.js
@@ -8,19 +8,39 @@ function login(req, res) {
   res.sendFile(path.join(__dirname, '..', 'client', 'login.html'))
 }
 
+function authorize(req, res) {
+  console.log(req.body)
+
+  // do some checking
+
+  res.cookie(cookieName, scrambler.encrypt('logged in'))
+  res.redirect("/")
+}
+
+function is_logged_in(req, res, next) {
+  const status = req.cookies[cookieName] ? scrambler.decrypt(req.cookies[cookieName]) : 'logged out'
+  console.log(status)
+  if(status == 'logged in') {
+    next()
+  } else {
+    res.redirect("/login")
+  }
+}
+
 function logout(req, res) {
   res.clearCookie(cookieName)
-  res.redirect("/")
+  res.redirect("/login")
 }
 
 exports.init = (app) => {
 
   // auth routes
   app.get("/login", login)
+  app.post("/login", authorize)
   app.get("/logout", logout)
 
   // main application
-  app.get("/", (req, res) => {
+  app.get("/", is_logged_in, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'index.html'))
   })
 }
