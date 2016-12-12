b0d059494f039a154a3717dbe502ac2eff38232b
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-12 11:32:13 -0500

    add basic login page

--- app/auth.js
+++ app/auth.js
@@ -1,10 +1,11 @@
 const path = require('path')
 const scrambler = require('./scrambler')
+const words = require('random-words');
 const cookieName = words({ exactly: 5, join: '-' })
 
 function login(req, res) {
-  res.cookie(cookieName, scrambler.encrypt('logged in'))
-  res.redirect("/")
+  // res.cookie(cookieName, scrambler.encrypt('logged in'))
+  res.sendFile(path.join(__dirname, '..', 'client', 'login.html'))
 }
 
 function logout(req, res) {
