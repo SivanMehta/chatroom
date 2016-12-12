a4bcd10c8ddcde1093f6c38fd7300cf08c25e840
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-11 23:12:58 -0500

    created basic navigation bar

--- src/main.js
+++ src/main.js
@@ -1,50 +1,87 @@
 // React
 import React from 'react'
 import { render } from 'react-dom'
-
-// Material UI needs
-import injectTapEventPlugin from 'react-tap-event-plugin'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-injectTapEventPlugin()
-
-// Custom Components
-import Greeting from './pieces/greeting'
-
-// Routing
 import { Router, Route, hashHistory } from 'react-router'
+
+// Material UI
+import injectTapEventPlugin from 'react-tap-event-plugin'; injectTapEventPlugin()
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import IconButton from 'material-ui/IconButton'
+import MenuButton from 'material-ui/svg-icons/navigation/menu'
+import ArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right'
+import Popover from 'material-ui/Popover'
+import Menu from 'material-ui/Menu'
+import MenuItem from 'material-ui/MenuItem'
+import Divider from 'material-ui/Divider'
+
 
 class Main extends React.Component {
   constructor(props) {
     super(props)
-  }
 
-  renderBody() {
-    return (
-      <div>
-        <Greeting />
-        { this.props.children }
-      </div>
-    )
+    this.state = {
+      logged: false
+    }
   }
 
   render() {
     return(
       <MuiThemeProvider>
-        { this.renderBody() }
+        <AppBar
+            title="chatroom"
+            iconElementLeft={ <Nav /> }
+          />
       </MuiThemeProvider>
     )
   }
 }
 
-class Otherwise extends React.Component {
-  constructor(props) { super(props) }
-  render() { return( <span>nothing else</span> ) }
+class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false
+    }
+  }
+
+  renderRooms() {
+    // in the future, this would return some recommended rooms, but for now we can hard code them
+    return [
+      <MenuItem primaryText = "John" />,
+      <MenuItem primaryText = "Paul" />,
+      <MenuItem primaryText = "George" />,
+      <MenuItem primaryText = "Ringo" />
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        <IconButton onTouchTap = { () => this.setState({open: true}) }>
+          <MenuButton/>
+        </IconButton>
+        <Popover
+          open = { this.state.open }
+          onRequestClose = { () => this.setState({open: false}) } >
+          <Menu>
+            <MenuItem primaryText = "Profile" />
+            <MenuItem primaryText = "Settings" />
+            <MenuItem primaryText = "Rooms"
+                      menuItems = { this.renderRooms() }
+                      rightIcon = {<ArrowDropRight />}/>
+            <Divider />
+            <MenuItem primaryText="Logout" />
+          </Menu>
+        </Popover>
+      </div>
+    )
+  }
 }
 
 render((
   <Router history = { hashHistory }>
     <Route path="/" component = { Main }>
-      <Route path="/chatroom" component = { Otherwise }/>
     </Route>
   </Router>
 ), document.getElementById('app'))
