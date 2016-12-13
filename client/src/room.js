b281818321ae3d7c1c85829c98018a083f1e9167
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-13 14:08:00 -0500

    fetch messages on component load

--- client/src/room.js
+++ client/src/room.js
@@ -20,9 +20,19 @@ export default class Room extends React.Component {
     this.state = {
       messages: []
     }
+
+  }
+
+  componentDidMount() {
+    this.fetchMessages()
   }
 
   componentWillReceiveProps() {
+    this.fetchMessages()
+  }
+
+  fetchMessages() {
+    console.log("here")
     // in the future, this will fetch messages from the server,
     // but for now we're just generating them on the fly
     fetch('/api/rooms/' + this.props.params.roomId, { credentials: 'same-origin' })
