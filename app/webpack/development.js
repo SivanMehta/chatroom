ea06045f235a18d5b4cf63dc992064925eb7fe35
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-16 17:44:31 -0500

    configure different webpack depending on environment

---
+++ app/webpack/development.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const webpack = require('webpack')
+const path = require('path')
+
+
+exports.init = (dirname) => {
+  const LANG = process.env.LANG || 'en-US'
+  const dist = path.join(dirname, 'client', 'public', 'dist')
+
+  return [
+  {
+    name: 'JS',
+    devtool: 'source-map',
+    entry: ['whatwg-fetch', path.join(dirname, 'client', 'src', 'app.js')],
+    output: {
+      filename: path.join(dist, 'build', 'bundle.js')
+    },
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.ProvidePlugin({
+          "react": "React",
+      })
+    ],
+
+    module: {
+      loaders: [{
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: [
+            /node_modules\/babel[\s\S]*/
+          ],
+          query: {
+            presets: [
+              require.resolve('babel-preset-es2015'),
+              require.resolve('babel-preset-react')
+            ]
+          }
+        }
+      ]}
+    }
+  ]
+}
