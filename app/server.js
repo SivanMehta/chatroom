bbeaecea922caa22388dbf547be6e4e25bc57978
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-18 17:12:45 -0500

    move server file

--- app/server.js
+++ app/server.js
@@ -1,16 +1,16 @@
-var express = require('express')
+const express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
 
 // logging
-var morgan = require('morgan')
+const morgan = require('morgan')
 app.use(morgan('dev'))
 
 // body and cookie parsing
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-var cookieParser = app.use(require('cookie-parser')())
+app.use(require('cookie-parser')())
 
 // Handle static files
 app.use(express.static(path.join(__dirname, 'client', 'public')))
@@ -20,13 +20,13 @@ app.set('port', PORT)
 var server = require('http').Server(app)
 
 // external routes
-require('./app/auth').init(app)
+require('./auth').init(app)
 
 /* Socket.io Communication */
 var io = require('socket.io').listen(server)
-var socketCookieParser = require('socket.io-cookie')
-io.use(socketCookieParser)
-require('./app/rooms').initializeSocket(io)
+const socketCookieParser = require('socket.io-cookie')
+io.use(socketCookieParser())
+require('./rooms').initializeSocket(io)
 
 server.listen(PORT, () => {
     console.log("Server started on port " + PORT)
