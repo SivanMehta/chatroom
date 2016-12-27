85f498e6e772bda30f72f3d3660337ee6eb14c14
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-26 19:22:37 -0500

    basic ui behind search page

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -6,20 +6,60 @@ import 'whatwg-fetch'
 // Material UI
 import CircularProgress from 'material-ui/CircularProgress'
 import { List, ListItem } from 'material-ui/List'
+import TextField from 'material-ui/TextField'
+
+// persist button
+import SearchIcon from 'material-ui/svg-icons/action/search'
+import DoneIcon from 'material-ui/svg-icons/action/done'
+import { green900 } from 'material-ui/styles/colors'
+import RaisedButton from 'material-ui/RaisedButton'
 
 export default class Search extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      query : "asuh dude"
+      query : '',
+      searching: 0
     }
+
+    this.fetchSearchResults = this.fetchSearchResults.bind(this)
+  }
+
+  renderIcon() {
+    switch (this.state.searching) {
+        case 0:
+          return <SearchIcon color = { green900 } />
+        case 1:
+          return <CircularProgress size = { 24 } color = { green900 } />
+        case 2:
+          return <DoneIcon color = { green900 } />
+      }
+  }
+
+  fetchSearchResults() {
+    this.setState({ searching: 1 })
+
+    // simulating searching
+    setTimeout(() => this.setState({ searching: 2 }), 5000)
   }
 
   render() {
     return(
       <div>
-        Search for stuff!
+        <TextField
+          id = 'query'
+          hintText = 'Search for Messages'
+          value = { this.state.query }
+          onChange={ event => this.setState({ query: event.target.value}) }
+          fullWidth = { true }/>
+        <br />
+        <RaisedButton
+          label = "Submit"
+          labelPosition = "before"
+          backgroundColor = "#C5E1A5"
+          icon = { this.renderIcon() }
+          onTouchTap = { this.fetchSearchResults }/>
       </div>
     )
   }
