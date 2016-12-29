012ac332a8807d8172e17fa0de23228d4a42f64d
Sivan Mehta <skmehta@andrew.cmu.edu>
2016-12-28 23:51:34 -0500

    use Array.prototype.map to programmatically define step
    this avoid a lot of repetitive typing and make it
    very easy to document what each step is doing

--- app/index.js
+++ app/index.js
@@ -2,28 +2,30 @@ const async = require('async')
 const express = require('express')
 var app = express()
 
-/*
-  Setup application
-  1. Initialize logger
-  2. Body and Cookie Parsing
-  3. Initialize database
-  4. Serve Static Files
-  5. Initialize server
-  5. Initialize socket connection
-  6. Initilize all of the routes
-  success: Start Server
-  failure: Log Error Message
- */
+// Instantiate intializers of the application
+const intializers = [
+  // 1. Initialize logger
+  './models/logger',
+  // 2. Body and Cookie Parsing
+  './models/parsing',
+  // 3. Initialize database
+  './database/database',
+  // 4. Serve Static Files
+  './models/static-files',
+  // 5. Initialize server
+  './models/server',
+  // 5. Initialize socket connection
+  './models/room-socket',
+  // 6. Initilize all of the routes
+  './routes'
+].map(filename => {
+  return (done) => { require(filename).init(app, done) }
+})
 
-async.waterfall([
-  (done) => { require('./models/logger').init(app, done) },
-  (done) => { require('./models/parsing').init(app, done) },
-  (done) => { require('./database/database').init(app, done) },
-  (done) => { require('./models/static-files').init(app, done) },
-  (done) => { require('./models/server').init(app, done) },
-  (done) => { require('./models/room-socket').init(app, done) },
-  (done) => { require('./routes').init(app, done) }
-], (err, _) => {
+// activate all intializers
+async.waterfall(intializers, (err, _) => {
+  // success: Start server
+  // failure: Log error Message
   if(err) {
     app.logger.error(error)
   } else {
@@ -32,22 +34,3 @@ async.waterfall([
     })
   }
 })
-
-// set up server
-// const PORT = process.env.PORT || 8080
-// app.set('port', PORT)
-// var server = require('http').Server(app)
-//
-// // external routes
-// require('./auth').init(app)
-//
-// // Socket.io Communication
-// var io = require('socket.io').listen(server)
-// var socketCookieParser = require('socket.io-cookie')
-// io.use(socketCookieParser)
-// require('./models/room').initializeSocket(io)
-//
-// // start service
-// server.listen(PORT, () => {
-//     logger.info("Server started on port " + PORT)
-// })
