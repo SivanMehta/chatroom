5b5050e5ff96a2dc053717edeba8ae2fbef77113
Sivan Mehta <skmehta@andrew.cmu.edu>
2017-01-01 18:52:05 -0500

    outline tests for future implementation

---
+++ test/api/messages.js
@@ -0,0 +1,8 @@
+var assert = require('assert')
+
+describe('Messages', () => {
+  it('should be able to see the messages in a specific room')
+  it('should be able to send a new message in a new room')
+  it('should be able to see your newly send message')
+  it('should emit socket:message via websockets on message sending')
+})
