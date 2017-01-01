6d5745490dd2dd4d23bd52d305663f3a8b465f2e
Sivan Mehta <skmehta@andrew.cmu.edu>
2017-01-01 16:17:14 -0500

    " -> '

--- app/routes.js
+++ app/routes.js
@@ -13,37 +13,37 @@ exports.init = (app, done) => {
     // do some checking
 
     res.cookie('email', req.body.email)
-    res.redirect("/")
+    res.redirect('/')
   }
 
   function is_logged_in(req, res, next) {
     const status = req.cookies.email ? true : false
-    status ? next() : res.redirect("/login")
+    status ? next() : res.redirect('/login')
   }
 
   function logout(req, res) {
     ['email', 'io'].forEach(cookie => res.clearCookie(cookie))
-    res.redirect("/login")
+    res.redirect('/login')
   }
 
   // auth routes
-  app.get("/login", login)
-  app.post("/login", authorizeBrowser)
-  app.get("/logout", logout)
+  app.get('/login', login)
+  app.post('/login', authorizeBrowser)
+  app.get('/logout', logout)
 
   // api-driven routes
   // app.get('/api/authorize', authorizeAPI)
-  app.get("/api/profiles", is_logged_in, profile.getProfile)
-  app.get("/api/settings", is_logged_in, profile.getSettings)
+  app.get('/api/profiles', is_logged_in, profile.getProfile)
+  app.get('/api/settings', is_logged_in, profile.getSettings)
   app.get('/api/messages/:roomID', is_logged_in, app.db.getRoomMessages)
-  app.post("/api/messages", is_logged_in, app.db.receiveMessage)
-  app.get("/api/search", is_logged_in, app.db.searchMessages)
+  app.post('/api/messages', is_logged_in, app.db.receiveMessage)
+  app.get('/api/search', is_logged_in, app.db.searchMessages)
 
   // serve react application
-  app.get("/", is_logged_in, (req, res) => {
+  app.get('/', is_logged_in, (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'index.html'))
   })
 
-  app.logger.info("Initialized routes")
+  app.logger.info('Initialized routes')
   done()
 }
